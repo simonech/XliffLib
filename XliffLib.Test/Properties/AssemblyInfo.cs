7b409984b51409479566c4873b418ae44f8e535f
Simone Chiaretta <simone@piyosailing.com>
2014-07-24 11:24:45 +0200

    Updated AssemblyInfos

--- XliffLib.Test/Properties/AssemblyInfo.cs
+++ XliffLib.Test/Properties/AssemblyInfo.cs
@@ -5,14 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XliffLib.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XliffLib.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Test library for Xliff.NET")]
+[assembly: AssemblyDescription("Test library for Xliff.NET")]
+[assembly: AssemblyProduct("Xliff.NET")]
+[assembly: AssemblyCopyright("Copyright © 2014 Simone Chiaretta")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
