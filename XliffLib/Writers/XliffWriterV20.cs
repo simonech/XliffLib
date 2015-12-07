71564faba01f92c44edfb300276e8a569e65460a
Simone Chiaretta <simone.chiaretta@consilium.europa.eu>
2015-12-07 12:20:35 +0100

    Added initial writer implementation for both v12 and v20

--- XliffLib/Writers/XliffWriterV20.cs
+++ XliffLib/Writers/XliffWriterV20.cs
@@ -10,5 +10,16 @@ namespace XliffLib.Writers
     public class XliffWriterV20: BaseXliffWriter
     {
         private static XNamespace xliffNS = "urn:oasis:names:tc:xliff:document:2.0";
+
+        public void Create(XliffDocument xliff)
+        {
+            ValidateXliff(xliff);
+            XDeclaration declaration = new XDeclaration("1.0", "utf-8", "");
+            XDocument root = new XDocument(declaration);
+            var xliffRoot = new XElement(xliffNS + "xliff");
+            xliffRoot.Add(new XAttribute("version", "2.0"));
+            root.Add(xliffRoot);
+            XliffDoc = root;
+        }
     }
 }
