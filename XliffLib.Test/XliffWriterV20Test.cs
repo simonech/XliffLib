3f5a907d5a747647daef118ba58c6a5af9cf20be
Simone Chiaretta <simone.chiaretta@consilium.europa.eu>
2015-12-07 11:49:50 +0100

    Moved specs into resources folder and added class for writing Xliff2.0 files

---
+++ XliffLib.Test/XliffWriterV20Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+using XliffLib.Writers;
+
+namespace XliffLib.Test
+{
+    [TestClass]
+    public class XliffWriterV20Test
+    {
+        private XliffWriterV20 SetupWriter()
+        {
+            return SetupWriter(new XliffDocument());
+        }
+        private XliffWriterV20 SetupWriter(XliffDocument doc)
+        {
+            var writer = new XliffWriterV20();
+            writer.Create(doc);
+            return writer;
+        }
+
+        [TestMethod]
+        public void CanGenerateXmlDoc()
+        {
+            var writer = SetupWriter();
+            Assert.IsNotNull(writer.XliffDoc);
+        }
+
+        [TestMethod]
+        public void XmlDocContainsDefinition()
+        {
+            var writer = SetupWriter();
+            string xliffDoc = writer.ToXmlString();
+            Assert.IsTrue(xliffDoc.Contains("<?xml"));
+        }
+
+        [TestMethod]
+        public void XmlDocContainsXliffSpecVersion()
+        {
+            var writer = SetupWriter();
+            string xliffDoc = writer.ToXmlString();
+            Assert.IsTrue(xliffDoc.Contains("<xliff version=\"2.0\""));
+        }
+    }
+}
---
+++ XliffLib/Writers/XliffWriterV20.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XliffLib.Writers
+{
+    public class XliffWriterV20: BaseXliffWriter
+    {
+    }
+}
