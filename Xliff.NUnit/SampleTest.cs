641d7ab6c27c69e89ea8cb75df8738ca670fb88b
Simone Chiaretta <simone@piyosailing.com>
2017-05-03 18:36:45 +0200

    Added NuUnit test and Xliff2.OM lib

---
+++ Xliff.NUnit/SampleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Xliff.NUnit
+{
+    [TestFixture] 
+    public class SampleTest
+    {
+        [Test]
+        public void CanAddTwoNumbers()
+        {
+            int expected = 4;
+            int result = 2 + 2;
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
