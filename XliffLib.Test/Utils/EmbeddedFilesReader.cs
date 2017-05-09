250a354818f01f37c5007d9270fe7877d3effb8d
Simone Chiaretta <simone@piyosailing.com>
2017-05-09 23:56:25 +0200

    More cleanup of project

--- XliffLib.Test/Utils/EmbeddedFilesReader.cs
+++ XliffLib.Test/Utils/EmbeddedFilesReader.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace XliffLib.Utils
+namespace XliffLib.Test.Utils
 {
     public static class EmbeddedFilesReader
     {
--- XliffTester/Program.cs
+++ XliffTester/Program.cs
@@ -4,10 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using XliffLib.Extractors;
 using XliffLib.Model;
-using XliffLib.Readers;
-using XliffLib.Writers;
 
 namespace XliffTester
 {
@@ -19,34 +16,16 @@ namespace XliffTester
             var directory = System.IO.Path.GetDirectoryName(path);
             string content = File.ReadAllText(Path.Combine(directory, "Samples", "original.txt"));
 
-            TextExtractor extractor = new TextExtractor();
-            var result = extractor.Extract(content);
 
             Bundle doc = new Bundle();
-            doc.Documents.Add(result.File);
 
-            XliffWriterV20 writer2 = new XliffWriterV20();
-            writer2.Create(doc,"en-us");
-            writer2.Save(Path.Combine(directory, "Samples", "original-v20.xml"));
 
-            File.WriteAllText(Path.Combine(directory, "Samples", "skeleton.txt"), result.Skeleton);
 
             //DisplayValidationErrors(reader3);
 
             Console.ReadLine();
         }
 
-        private static void DisplayValidationErrors(BaseXliffReader reader)
-        {
-            if (!reader.IsValid)
-            {
-                foreach (var error in reader.ValidationErrors)
-                {
-                    Console.WriteLine("[{0} - {1} ({2},{3})] {4}", error.Type, error.Severity, error.LineNumber, error.ColNumber, error.ErrorMessage);
-                }
-            }
-            else
-                Console.WriteLine("All good!");
-        }
+
     }
 }
