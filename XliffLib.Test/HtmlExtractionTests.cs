23ccc8d581fb0f56e8a26bee097b6ee94d84305d
Simone Chiaretta <simone@piyosailing.com>
2017-05-12 01:09:01 +0200

    Added method to identify HTML in strings

---
+++ XliffLib.Test/HtmlExtractionTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using XliffLib.Utils;
+using System.Collections;
+
+namespace XliffLib.Test
+{
+    [TestFixture()]
+    public class HtmlExtractionTests
+    {
+        [Test(),TestCaseSource(typeof(MyDataClass), "TestCases")]
+        public bool CanIdentifyHtmlInStrings(string text)
+        {
+            return text.IsHtml();
+        }
+    }
+
+	public class MyDataClass
+	{
+		public static IEnumerable TestCases
+		{
+			get
+			{
+				yield return new TestCaseData("Hello <b>World</b>!").Returns(true);
+				yield return new TestCaseData("Hello world!").Returns(false);
+				yield return new TestCaseData("<li>Item</li>").Returns(true);
+			}
+		}
+	}
+}
---
+++ XliffLib/Utils/HtmlExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace XliffLib.Utils
+{
+    public static class HtmlExtensions
+    {
+        public static bool IsHtml(this string text)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(text);
+            return !doc.DocumentNode.ChildNodes.All(n=>n.NodeType==HtmlNodeType.Text);
+        }
+    }
+}
