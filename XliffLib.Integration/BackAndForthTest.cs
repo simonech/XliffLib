feff4522d263a1f96f3f86621792ec26af6da514
Simone Chiaretta <simone@piyosailing.com>
2017-08-23 23:54:39 +0200

    Added back and forth test
    Loading a bundle from Json converting to xliff and back to json

---
+++ XliffLib.Integration/BackAndForthTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Localization.Xliff.OM.Core;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using XliffLib.Integration.Utils;
+using XliffLib.Utils;
+
+namespace XliffLib.Integration
+{
+    [TestFixture()]
+    public class BackAndForthTest
+    {
+        [Test(), TestCaseSource(typeof(DataSamples), "FileNames")]
+        public void CanExtractAndMerge(string filename)
+        {
+            var bundleString = EmbeddedFilesReader.ReadString("XliffLib.Integration.TestFiles." + filename + ".json");
+            var bundle = bundleString.ToBundle();
+
+            var extractor = new SimpleExtractor();
+            var xliffModel = extractor.Extract(bundle, "en-US", "it-IT");
+
+            foreach (var unit in xliffModel.CollapseChildren<Unit>())
+            {
+                foreach (var res in unit.Resources)
+                {
+                    res.Target = new Target();
+                    foreach (var source in res.Source.Text)
+                    {
+                        if(source is PlainText)
+                            res.Target.Text.Add(new PlainText(((PlainText)source).Text));
+                        if (source is CDataTag)
+                            res.Target.Text.Add(new CDataTag(((CDataTag)source).Text));
+                    }
+                }
+            }
+
+            var merger = new SimpleMerger();
+
+            var resultingBundle = merger.Merge(xliffModel);
+            var jsonResult = resultingBundle.ToJson();
+
+            JObject expected = JObject.Parse(bundleString);
+            JObject result = JObject.Parse(jsonResult);
+
+
+            Assert.IsTrue(JToken.DeepEquals(expected, result), "The two bundles are different:\r\nExpected {0}\r\nResult {1}", bundle, jsonResult);
+        }
+    }
+}
